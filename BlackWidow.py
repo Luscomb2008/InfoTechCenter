8987f0b0a43dc03dc85921b1f985c325eb6ce812
Luscomb <02lucas.luscomb@gulllakecs.org>
2025-02-10 13:57:01 -0500

    Updated VRS() - Blizzard - Stable

--- BlackWidow.py
+++ BlackWidow.py
@@ -17,6 +17,10 @@ weatherAlert= weather()
 def vehicleResponseSystem():
     if weatherAlert == "snowing":
         print("\nThe Nation Weather Service has updated our alarm by 30 minutes because"
-        "of the forecast of", weatherAlert, "weather condition.")
+            "of the forecast of", weatherAlert, "outside.")
+    elif weatherAlert == "blizzard":
+        print("\nThe Nation Weather Service has updated our alarm by 60 minutes because"
+            "of the forecast of", weatherAlert, "outside.")
+
 
 vehicleResponseSystem()
