7d61f2a5a4f3d06e3580686dad054a9d0542f3bb
Luscomb <02lucas.luscomb@gulllakecs.org>
2025-02-10 14:09:18 -0500

    chat GPT added comments

--- BlackWidow.py
+++ BlackWidow.py
@@ -1,39 +1,43 @@
+# Print initial lines with some separation and title
 print("\n*****************************************\n")
-
 print("Weather Branch - Developer: Lucas Luscomb\n")
 
-#Import Libraries Here!
-import random
-from time import sleep
+# Import necessary libraries
+import random  # For generating random weather conditions
+from time import sleep  # For creating time delays (though not used in the code)
 
-#Weather Function to determine the weather
+# Weather Function to determine the weather condition randomly
 def weather():
+    # List of possible weather conditions
     weatherForecastList = ["snowing", "blizzard", "icy", "rainy", "windy", "sunny"]
+    # Randomly choose one weather condition from the list
     weatherCondition = random.choice(weatherForecastList)
-    return weatherCondition
+    return weatherCondition  # Return the selected weather condition
 
-weatherAlert= weather()
+# Call the weather function to get a random weather alert
+weatherAlert = weather()
 
+# Define the vehicle response system that reacts to different weather conditions
 def vehicleResponseSystem():
+    # Check different weather conditions and print corresponding messages
     if weatherAlert == "snowing":
         print("\nThe Nation Weather Service has updated our alarm by 30 minutes because"
-            "of the forecast of", weatherAlert, "outside.")
+              " of the forecast of", weatherAlert, "outside.")
     elif weatherAlert == "blizzard":
         print("\nThe Nation Weather Service has updated our alarm by 60 minutes because"
-            "of the forecast of", weatherAlert, "outside.")
+              " of the forecast of", weatherAlert, "outside.")
     elif weatherAlert == "icy":
         print("\nThe Nation Weather Service has updated our alarm by 60 minutes because"
-            "of the forecast of", weatherAlert, "outside.")
+              " of the forecast of", weatherAlert, "outside.")
     elif weatherAlert == "rainy":
         print("\nThe Nation Weather Service has updated our alarm by 10 minutes because"
-            "of the forecast of", weatherAlert, "outside.")
+              " of the forecast of", weatherAlert, "outside.")
     elif weatherAlert == "windy":
         print("\nThe Nation Weather Service has updated our alarm by 5 minutes because"
-            "of the forecast of", weatherAlert, "outside.")
+              " of the forecast of", weatherAlert, "outside.")
     elif weatherAlert == "sunny":
         print("\nThe Nation Weather Service has predicted"
-            " the forecast of", weatherAlert, "outside.")
+              " the forecast of", weatherAlert, "outside.")
 
-
-
+# Call the vehicle response system to output the alarm update based on the current weather
 vehicleResponseSystem()
