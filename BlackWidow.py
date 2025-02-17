3dc812448fb0e3ed76d9c72111c964fea1cdba95
Luscomb <02lucas.luscomb@gulllakecs.org>
2025-02-17 13:52:56 -0500

    Added gas level Alert Function- STABLE

--- BlackWidow.py
+++ BlackWidow.py
@@ -13,8 +13,28 @@ def gasStations():
     return random.choice(gasStationsList)
 
 
-print(GasLevelGauge())
-print(gasStations())
+def gaslevelalert():
+    milesToGasStationLow = round(random.uniform(1,25,),1)
+    milesToGasStationQaurter = round(random.uniform(25.1,50),1)
+    
+    gaslevelindicator = GasLevelGauge()
+    if gaslevelindicator == "Empty":
+        print("******WARNING******  YOU ARE OUT OF GAS\n")
+        sleep(1.25)
+        print("calling AAA")
+    elif gaslevelindicator == "Low":
+        print("Your gas tank is extremly low, using gps for the closest gas station...")
+        sleep(1.25)
+        print("the closes gas station is", gasStations(), "which is", milesToGasStationLow, "miles away.")
+    elif gaslevelindicator == "Low":
+        print("Your gas tank is at a qaurter tank, using gps for the closest gas station...")
+        sleep(1.25)
+        print("the closes gas station is", gasStations(), "which is", milesToGasStationLow, "miles away.")
+
+gaslevelalert()
+
+
+
 
 
 
